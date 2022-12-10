c340be386a70e19b645474f75131400b84c58219
luisisllouise <1903626029@qq.com>
2022-12-10 23:02:55 +0800

    feat: langList add Chinese and Korean

--- src/plugins/VueI18n.ts
+++ src/plugins/VueI18n.ts
@@ -1,8 +1,9 @@
 import { createI18n } from "vue-i18n";
 
 import en_us from "@/lang/en_us";
-
-const langList = { en_us };
+import zh_cn from "@/lang/zh_cn";
+import ko_kr from "@/lang/ko_kr";
+const langList = { en_us,zh_cn,ko_kr };
 
 const i18n = createI18n({
     globalInjection: true,
